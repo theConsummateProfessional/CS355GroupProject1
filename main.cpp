b2ddfac616fc5eb58232e10c2e9267c6fc5a6315
pbyars <60798194+pbyars@users.noreply.github.com>
2020-02-07 15:52:22 -0600

    Added the main driver

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+void printMenu();
+
+int main()
+{
+	Stack<int> stack;
+	char choice = ' ';
+	int value = 0;
+
+	printMenu();
+	
+	cin >> choice;
+	cout << endl;
+	do
+	{
+		switch(choice)
+		case +:
+			cout << "Enter a value to be added: ";
+			cin >> value;
+			stack.push(value);
+			break;
+
+		case -:
+			stack.pop();
+			break;
+
+		case ?: 
+
+			break;
+
+		case P:
+
+			break;
+
+		case @:
+
+			break;
+
+		case 1:
+
+			break;
+
+		case 2:
+
+			break;
+
+		case 3:
+
+			break;
+
+		case M:
+			printMenu();
+			break;
+
+		case Q:
+			cout << "Goodbye..." << endl;
+			break;
+
+		default: 
+			cout << "Incorrect input of a choice from the menu. Try again..." << endl;
+			break;
+	}
+	while(choice != 'Q');
+
+	return 0;
+}
+
+void printMenu()
+{
+	cout << "+  Insert an item" << endl
+		"-  Remove an item" << endl
+		"?  Search for an item" << endl
+		"P  Print the list" << endl
+		"@  Print the item at the cursor" << endl
+		"1  Test copy constructor" << endl
+		"2  Test assignment operator" << endl
+		"3  Test destructor" << endl
+		"M  Print this menu again" << endl
+		"Q  End this program" << endl << endl
+		"Make a choice from the characters above: ";
+}
