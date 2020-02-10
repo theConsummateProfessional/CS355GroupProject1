e07ace3e1f10bb751e8934e6d70568bf58e66b08
pbyars <60798194+pbyars@users.noreply.github.com>
2020-02-10 15:57:52 -0600

    Fixed typeNode<> errors

--- stackList.h
+++ stackList.h
@@ -5,20 +5,20 @@ class typeNode
 {
 	private:
 		Type data;
-		Type* ptr;
+		typeNode<Type>* ptr;
 	public:
-		typeNode(Type theData, Type* theLink);
+		typeNode(Type theData, typeNode<Type>* theLink);
 		void setData(Type theData);
 		void setLink(Type* theLink);
 		Type getData() const;
-		Type* getLink() const;
+		typeNode<Type>* getLink() const;
 };
 
 template <class Type>
 class Stack
 {
 	private:
-		Type* top;
+		typeNode<Type>* top;
 	public:
 		Stack();
 		~Stack();
@@ -27,4 +27,108 @@ class Stack
 		bool empty() const;
 		bool full() const;
 };
+
+
+template <class Type>
+typeNode<Type>::typeNode(Type theData, typeNode<Type>* theLink)
+{
+	data = theData;
+	ptr = theLink;
+	//current = false;
+}
+
+template <class Type>
+void typeNode<Type>::setData(Type theData)
+{
+	data = theData;
+}
+
+template <class Type>
+void typeNode<Type>::setLink(Type* theLink)
+{
+	ptr = theLink;
+}
+
+/*
+template <class Type>
+void setCurrent(bool cur)
+{
+	current = cur;
+}
+*/
+
+template <class Type>
+Type typeNode<Type>::getData() const
+{
+	return data;
+}
+
+template <class Type>
+typeNode<Type>* typeNode<Type>::getLink() const
+{
+	return ptr;
+}
+
+/*
+template <class Type>
+bool typeNode<Type>::isCurrent() const
+{
+	return current;
+}
+*/
+
+template <class Type>
+Stack<Type>::Stack()
+{
+	top = NULL;
+}
+
+template <class Type>
+Stack<Type>::~Stack()
+{
+	while(!empty())
+		cout << pop() << endl;
+}
+
+template <class Type>
+void Stack<Type>::push(Type inData)
+{
+	if(!full())
+	{
+		typeNode<Type>* temp = new typeNode<Type>(inData, top);
+		top = temp;
+	}
+}
+
+template <class Type>
+Type Stack<Type>::pop()
+{
+	Type value;
+	if(empty())
+		cout << "Cannot pop from an empty list" << endl;
+	else
+	{
+		typeNode<Type>* temp = top;
+		value = temp -> getData();
+		top = temp -> getLink();
+		//top->isCurrent(true);
+		delete temp;
+	}
+	return value;
+}
+
+template <class Type>
+bool Stack<Type>::empty() const
+{
+	if (top == NULL)
+		return true;
+	return false;
+
+}
+
+template <class Type>
+bool Stack<Type>::full() const
+{
+	return false;
+}
 
